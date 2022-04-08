95f2b2612df397782041c1aa9627052f84452f14
jasonjcchang <jasonjcchang@kkstream.com>
2022-04-08 23:23:40 +0800

    feat: update SearchBar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,20 +1,21 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import { debounce } from "lodash-es";
-import { memo, useCallback } from "react"
+import { memo, useCallback, useEffect } from "react";
 
-const gray = "#e8e8e8"
+const gray = "#e8e8e8";
 
 const container = css`
   text-align: left;
-`
+  padding: 16px;
+`;
 
 const search = css`
   height: 35px;
   width: 200px;
   border: 1px solid ${gray};
   border-radius: 1px;
-`
+`;
 
 interface Props {
   onSearch: (text: string) => void;
@@ -23,14 +24,30 @@ interface Props {
 
 const SearchBar = (props: Props) => {
   const { onSearch, defaultValue } = props;
-  const debouncedChange = useCallback((event) => {
-    debounce((event) => {
-      onSearch((event.target as HTMLInputElement).value)
-    }, 1000)(event)
-  }, [onSearch]);
-  return <div css={container}>
-    <input defaultValue={defaultValue} css={search} type="search" placeholder='Please enter the keyword' onChange={debouncedChange} />
-  </div>
-}
 
-export default memo(SearchBar)
+  const debouncedChange = useCallback(
+    debounce((event) => {
+      onSearch((event.target as HTMLInputElement).value);
+    }, 1000),
+    [onSearch]
+  );
+
+  useEffect(() => {
+    return () => {
+      debouncedChange.cancel();
+    };
+  }, [debouncedChange]);
+  return (
+    <div css={container}>
+      <input
+        defaultValue={defaultValue}
+        css={search}
+        type="search"
+        placeholder="Please enter the keyword"
+        onChange={debouncedChange}
+      />
+    </div>
+  );
+};
+
+export default memo(SearchBar);
